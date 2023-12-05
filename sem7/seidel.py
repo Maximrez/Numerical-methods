a4cf56141afbb19114046ac1066594123a64ed25
Maximrez <maximrez@mail.ru>
2023-12-05 09:28:03 +0300

    laba 2

--- sem7/seidel.py
+++ sem7/seidel.py
@@ -29,25 +29,63 @@ def iter_seidel(A: np.ndarray, b: np.ndarray, result: np.ndarray):
 
 
 # delta = 1e-14
-def solve_seidel(A: np.ndarray, b: np.ndarray, error: int, delta=1e-14):
+def solve_seidel(A: np.ndarray, b: np.ndarray, error: int=0, delta=1e-14):
     result = np.zeros((n, 1))
     for i in range(n):
         result[i][0] = b[i][0] / A[i][i]
+
+    iter_count = 0
     while (errors(x0, iter_seidel(A, b, result))[error] >= delta):
         result = iter_seidel(A, b, result)
-    return result
+        iter_count += 1
+    return result, iter_count
 
 
-print(solve_seidel(A2, b2, 1))
+# cur = 10
+# count = 10
+# step = 1.5
+#
+# x = np.zeros(count)
+# y = np.zeros((3, count))
+# for i in range(count):
+#     n = cur
+#     x[i] = cur
+#
+#     A1 = np.random.random((n, n)) * 2 - 1 + np.identity(n) * (np.random.random((n, n)) + n + 1)
+#     b1 = np.random.random((n, 1)) * 2 - 1
+#     A2 = np.copy(A1)
+#     b2 = np.copy(b1)
+#     Atest = np.copy(A1)
+#     btest = np.copy(b1)
+#
+#     x0 = np.linalg.solve(A1, b1)
+#     for j in range(3):
+#         start = time.time()
+#         solve_seidel(A2, b2, j)
+#         end = time.time()
+#         y[j][i] = (end - start) * 10 ** 3
+#
+#     cur = int(step * cur)
 
-cur = 10
-count = 10
+# plt.figure(figsize=(12, 8))
+# plt.loglog(x, y[0])
+# plt.loglog(x, y[1])
+# plt.loglog(x, y[2])
+# plt.grid()
+# plt.title('Time')
+# plt.xlabel('Matrix size')
+# plt.ylabel('Time, ms')
+# plt.legend(['maxmod', 'delta_result', 'delta_x0'])
+# plt.show()
+
+n = 200
+cur = 1e-16
+count = 50
 step = 1.5
 
 x = np.zeros(count)
-y = np.zeros((3, count))
+y = np.zeros(count)
 for i in range(count):
-    n = cur
     x[i] = cur
 
     A1 = np.random.random((n, n)) * 2 - 1 + np.identity(n) * (np.random.random((n, n)) + n + 1)
@@ -58,21 +96,15 @@ for i in range(count):
     btest = np.copy(b1)
 
     x0 = np.linalg.solve(A1, b1)
-    for j in range(3):
-        start = time.time()
-        solve_seidel(A2, b2, j)
-        end = time.time()
-        y[j][i] = (end - start) * 10 ** 3
+    y[i] = solve_seidel(A2, b2, delta=cur)[1]
 
-    cur = int(step * cur)
+    cur *= step
 
 plt.figure(figsize=(12, 8))
-plt.loglog(x, y[0])
-plt.loglog(x, y[1])
-plt.loglog(x, y[2])
+plt.semilogx(x, y)
 plt.grid()
-plt.title('Time')
-plt.xlabel('Matrix size')
-plt.ylabel('Time, ms')
-plt.legend(['maxmod', 'delta_result', 'delta_x0'])
+plt.title('Steps')
+plt.xlabel('delta')
+plt.ylabel('steps')
+plt.legend(['maxmod'])
 plt.show()
