c9fd941040cc25f4ea9b5b06e72147bc43d77dcb
Maximrez <maximrez@mail.ru>
2023-12-12 09:01:13 +0300

    laba 3

--- sem7/reyleigh.py
+++ sem7/reyleigh.py
@@ -8,10 +8,8 @@ warnings.filterwarnings("ignore", category=SparseEfficiencyWarning)
 
 
 def U(x):
-    if x < 1 / 4:
-        return 0
-    elif x < 3 / 4:
-        return 1000
+    if abs(x - 0.5) < 0.25:
+        return -500
     else:
         return 0
 
@@ -20,45 +18,91 @@ N = 1001
 l = 1
 h = l / N
 A = np.zeros((N - 1, N - 1))
-A[0][0] = U(h) + 2 / h ** 2
+x = np.linspace(0, l, N - 1)
+A[0][0] = U(x[0]) + 2 / h ** 2
 A[0][1] = -1 / h ** 2
-A[N - 2][N - 2] = U((N - 1) * h) + 2 / h ** 2
-A[N - 2][N - 3] = -1 / h ** 2
+A[-1][-1] = U(x[-1]) + 2 / h ** 2
+A[-1][-2] = -1 / h ** 2
 for i in range(1, N - 2):
-    A[i][i] = U((i + 1) * h) + 2 / h ** 2
+    A[i][i] = U(x[i]) + 2 / h ** 2
     A[i][i + 1] = -1 / h ** 2
     A[i][i - 1] = -1 / h ** 2
 
 
-def rayleigh(A, epsilon, mu, b):
-    y = ssl.spsolve(A - mu * np.identity(A.shape[0]), b)
-    b = y / np.linalg.norm(y)
-    mu = np.dot(np.dot(np.reshape(b, (1, N - 1)), A), b) / (np.dot(np.reshape(b, (1, N - 1)), b))
-    err = np.max(np.abs(mu * b - np.dot(A, b)))
+def rayleigh_method(A, b_norm, eps: float = 1e-6, mu: float = 100.0):
+    diff = 1e-1
+    while eps < diff:
+        b_new = ssl.spsolve(A - mu * np.identity(b_norm.shape[0]), b_norm)
+        b_norm = b_new / np.linalg.norm(b_new)
+        mu = (A @ b_norm) @ b_norm
+        diff = np.max(np.abs(A @ b_norm - mu * b_norm))
 
-    while err > epsilon:
-        y = ssl.spsolve(A - mu * np.identity(A.shape[0]), b)
-        b = y / np.linalg.norm(y)
-        mu = np.dot(np.dot(np.reshape(b, (1, N - 1)), A), b) / (np.dot(np.reshape(b, (1, N - 1)), b))
-        err = np.max(np.abs(mu * b - np.dot(A, b)))
-
-    return mu, b
+    return b_norm, mu
 
 
-b = np.reshape(np.ones(N - 1), (N - 1, 1))
-b[N // 2:] *= -1
-result = rayleigh(A, 1e-7, 0, b)
-print(*result[0])
+fig, ax = plt.subplots(figsize=(12, 8))
+ax.set_title('Eigenfunction')
+ax.set_xlabel("x")
+ax.set_ylabel("y")
 
-x = np.linspace(0, 1, N + 1, endpoint=True)
-y = np.reshape(result[1], (1, N - 1)).flatten()
-y = np.insert(y, 0, 0)
-y = np.append(y, 0)
+eigenvalues = list()
 
-plt.figure(figsize=(12, 8))
-plt.plot(x, y)
+# 0
+b0 = np.reshape(np.ones(N - 1), (N - 1, 1))
+ans = rayleigh_method(A, b0, mu=-500)
+eigenvalue = ans[1]
+print(eigenvalue)
+eigenvalues.append(eigenvalue)
+y = ans[0]
+ax.plot(x, y)
+
+# 1
+b0 = np.reshape(np.ones(N - 1), (N - 1, 1))
+b0[N // 2:] *= -1
+ans = rayleigh_method(A, b0, mu=-1000)
+eigenvalue = ans[1]
+print(eigenvalue)
+eigenvalues.append(eigenvalue)
+y = ans[0]
+ax.plot(x, y)
+
+# 2
+b0 = np.reshape(np.ones(N - 1), (N - 1, 1))
+ans = rayleigh_method(A, b0, mu=-4000)
+eigenvalue = ans[1]
+print(eigenvalue)
+eigenvalues.append(eigenvalue)
+y = ans[0]
+ax.plot(x, y)
+
+# 3
+b0 = np.reshape(np.ones(N - 1), (N - 1, 1))
+b0[N // 2:] *= -1
+ans = rayleigh_method(A, b0, mu=-4000)
+eigenvalue = ans[1]
+print(eigenvalue)
+eigenvalues.append(eigenvalue)
+y = ans[0]
+ax.plot(x, y)
+
+# 4
+b0 = np.ones((N - 1)) / np.linalg.norm(np.ones((N - 1, 1)))
+ans = rayleigh_method(A, b0, mu=500)
+eigenvalue = ans[1]
+print(eigenvalue)
+eigenvalues.append(eigenvalue)
+y = ans[0]
+ax.plot(x, y)
+
+# 10
+# b0 = np.reshape(np.ones(N - 1), (N - 1, 1))
+# ans = rayleigh_method(A, b0, mu=1000)
+# eigenvalue = ans[1]
+# print(eigenvalue)
+# eigenvalues.append(eigenvalue)
+# y = ans[0]
+# ax.plot(x, y)
+
+plt.legend(eigenvalues)
 plt.grid()
-plt.title("СФ")
-plt.xlabel("x")
-plt.ylabel("СФ")
 plt.show()
